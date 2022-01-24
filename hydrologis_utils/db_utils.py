3af8b8b345a3f4dfa6316162c29a007629724bd5
Andrea Antonello <andrea.antonello@gmail.com>
2022-01-24 17:25:28 +0100

    testing with sqlite

--- hydrologis_utils/db_utils.py
+++ hydrologis_utils/db_utils.py
@@ -35,12 +35,18 @@ class DbType(Enum):
 
 class DbColumn:
     def __init__(self, **kwargs):
-        self.name = kwargs['name']
-        self.type = kwargs['type']
-        self.is_nullable = kwargs['nullable']
-        self.default = kwargs['default']
-        self.is_autoincrement = kwargs['autoincrement']
-        self.comment = kwargs['comment']
+        if 'name' in kwargs:
+            self.name = kwargs['name']
+        if 'type' in kwargs:
+            self.type = kwargs['type']
+        if 'nullable' in kwargs:
+            self.is_nullable = kwargs['nullable']
+        if 'default' in kwargs:
+            self.default = kwargs['default']
+        if 'autoincrement' in kwargs:
+            self.is_autoincrement = kwargs['autoincrement']
+        if 'comment' in kwargs:
+            self.comment = kwargs['comment']
         self.geoinfo = None
         if isinstance(self.type, Geometry):
             self.geoinfo = GeoInfo(self.type)
@@ -75,7 +81,7 @@ class ADb(ABC):
     def get_db_info(self):
         pass
 
-    def get_tables(self, do_order=False, schema='public'):
+    def get_tables(self, do_order=False, schema=None):
         table_names = self.engine.table_names(schema=schema)
         if do_order:
             table_names = sorted(table_names)
@@ -83,6 +89,19 @@ class ADb(ABC):
         # engine = create_engine('...')
         # insp = inspect(engine)
         # print(insp.get_table_names())
+    
+    def get_views(self, do_order=False, schema=None):
+        inspector = inspect(self.engine)
+        views = inspector.get_view_names()
+        if do_order:
+            views = sorted(views)
+        return views
+
+    def has_view(self, view_name, schema=None):
+        views = self.get_views(schema=schema)
+        if view_name in views:
+            return True
+        return False
 
     def has_table(self, table_name, schema=None):
         return self.engine.has_table(table_name=table_name, schema=schema)
@@ -97,17 +116,44 @@ class ADb(ABC):
         columns = inspector.get_columns(table_name)
         db_cols = [ DbColumn(**item) for item in columns ]
         return db_cols
+    
+    def get_geometry_column(self, table_name):
+        """Get the geometry columns of a table.
+        
+        :param table_name: the name of the table to get the geometry column from. 
+        :return: the geometry column or None.
+        """
+        inspector = inspect(self.engine)
+        columns = inspector.get_columns(table_name)
+        for column in columns:
+            dbcolumn = DbColumn(**column)
+            if dbcolumn.geoinfo:
+                return dbcolumn
 
-    def query(self, sql_string):
+    def execute(self, sql_string):
         with self.engine.connect() as conn:
             result = conn.execute(text(sql_string))
             return result
     
-    def get_table(self, table_name):
+    def get_table_data(self, table_name, order_by=None, limit=None, where=None):
         """Return the content of a table.
+
+        :param table_name: the table to list data from.
+        :param order_by: optional parameter to order the data (name of columns to order by).
+        :param limit: optional parameter to limit the return count.
+        :param where: optional where clause.
         """
+        sql = f"select * from {table_name}"
+
+        if where:
+            sql += " where " + where
+        if order_by:
+            sql += " order by " + order_by
+        if limit:
+            sql += f" limit {limit}"
+
         with self.engine.connect() as conn:
-            result = conn.execute(text(f"select * from {table_name}"))
+            result = conn.execute(text(sql))
             return result
     
     def create_table(self, table_object):
@@ -115,6 +161,18 @@ class ADb(ABC):
     
     def drop_table(self, table_object):
         table_object.drop(self.engine)
+    
+    def drop_table(self, table_name):
+        """Drop a table or view by its name.
+        """
+        geometry_col = self.get_geometry_column(table_name)
+        with self.engine.connect() as conn:
+            trans = conn.begin()
+            if geometry_col:
+                conn.execute(text(f"select DropGeometryColumn('public','{table_name}', '{geometry_col.name}');"))
+            result = conn.execute(text(f"drop table if exists {table_name} cascade"))
+            trans.commit()
+            return result
     
     def insert(self, table_object, data_list):
         with self.engine.connect() as conn:
@@ -139,4 +197,10 @@ class PostgresDb(ADb):
     def get_db_info(self):
         res = self.query(
             "SELECT VERSION() as pgversion, PostGIS_Full_Version() as pgisversion;")
+        return [res.pgversion, res.pgisversion]
+
+class SqliteDb(ADb):
+    def get_db_info(self):
+        res = self.query(
+            "SELECT sqlite_version() as sqliteversion;")
         return [res.pgversion, res.pgisversion]
--- tests/test_db_utils.py
+++ tests/test_db_utils.py
@@ -10,6 +10,14 @@ import unittest
 # run with python3 -m unittest discover tests/
 
 class TestDb(unittest.TestCase):
+
+    def setUp(self):
+        self.url = DbType.SQLITE_MEM.url()
+
+        self.db = SqliteDb(self.url, echo=False)
+        self.metadata = MetaData()
+            
+
     def test_col(self):
         col = DbColumn(name="test", is_autoincrement=False, blah="blu")
         self.assertEqual(col.name, "test")
@@ -22,77 +30,101 @@ class TestDb(unittest.TestCase):
         col = DbColumn(**args)
         self.assertEqual(col.name, "test")
 
+
+
     
-    def test_postgres(self):
-        db_host = 'localhost'
-        db_port = '5432'
-        db_name = 'test'
-        db_user = 'postgres'
-        db_password = 'postgres'
+    def test_sqlite(self):
 
-        url = DbType.POSTGRESQL.url(dbname=db_name, host=db_host, port=db_port, user=db_user, pwd=db_password)
-
-        db = PostgresDb(url, echo=False)
-        tables = db.get_tables(do_order=True)
-        # for tabel in tables:
-        #     print(tabel)
-
-        tname = "django_content_type"
-
-        cols = db.get_table_columns(tname)
-        print(f"COLUMNS: {tname}")
-        for col in cols:
-            print(col)
         
-        result = db.get_table(tname)
-        # for row in result:
-        #     print(row)
-
-        metadata = MetaData()
-        lake_table = Table('lake', metadata,
+        table_name = 'test'
+        test_table = Table(table_name, self.metadata,
             Column('id', Integer, primary_key=True),
-            Column('name', String),
-            Column('geom', Geometry('POLYGON', srid=4326))
+            Column('name', String)
         )
+        # test_table = Table('test', metadata,
+        #     Column('id', Integer, primary_key=True),
+        #     Column('name', String),
+        #     Column('geom', Geometry('POLYGON', srid=4326))
+        # )
 
-        if db.has_table(lake_table.name):
-            db.drop_table(lake_table)
+        self.assertFalse(self.db.has_table(table_name))
 
-        db.create_table(lake_table)
+        self.db.create_table(test_table)
 
-        cols = db.get_table_columns(lake_table.name)
-        print(f"COLUMNS: {lake_table.name}")
-        for col in cols:
-            print(col)
-        
+        self.assertTrue(self.db.has_table(table_name))
+
+
+        tables = self.db.get_tables(do_order=True)
+        self.assertEquals(len(tables), 1)
+        self.assertEquals(tables[0], table_name)
+
+        cols = self.db.get_table_columns(table_name)
+        self.assertEquals(len(cols), 2)
+
         dataList = [
-            {'name': 'Majeur', 'geom': 'SRID=4326;POLYGON((0 0,1 0,1 1,0 1,0 0))'},
-            {'name': 'Garde', 'geom': 'SRID=4326;POLYGON((1 0,3 0,3 2,1 2,1 0))'},
-            {'name': 'Orta', 'geom': 'SRID=4326;POLYGON((3 0,6 0,6 3,3 3,3 0))'}
+            {'name': 'Majeur'},
+            {'name': 'Garde'},
+            {'name': 'Orta'}
         ]
-        db.insert(lake_table, dataList)
+        self.db.insert(test_table, dataList)
 
-        #  conn.execute(Poi.__table__.insert(), [
-        #     {'geog': 'SRID=4326;POINT(1 1)'},
-        #     {'geog': WKTElement('POINT(1 1)', srid=4326)},
-        #     {'geog': WKTElement('SRID=4326;POINT(1 1)', extended=True)},
-        #     {'geog': from_shape(Point(1, 1), srid=4326)}
-        # ])
-
-
-        sel_obj  = select([lake_table])
-        result = db.select(sel_obj)
+        result = self.db.get_table_data(test_table)
+        count  = 0
         for row in result:
-            geom = to_shape(row['geom'])
-            area = db.decode(row['geom'].ST_Area())
-            wkt = db.decode(row['geom'].ST_AsEWKT())
-            print(f"{row[0]} - {geom}  - {area} - {wkt}")
+            count += 1
+        self.assertEquals(count, 3)
+
+        result = self.db.get_table_data(test_table, limit=2)
+        count  = 0
+        for row in result:
+            count += 1
+        self.assertEquals(count, 2)
         
-        # result = db.query("select * from lake")
+        result = self.db.get_table_data(test_table, where="name='Orta'")
+        count  = 0
+        for row in result:
+            count += 1
+        self.assertEquals(count, 1)
+        
+        
+
+        # views = db.get_views(do_order=True)
+        # for view in views:
+        #     print(view)
+
+        # cols = db.get_table_columns(lake_table.name)
+        # print(f"COLUMNS: {lake_table.name}")
+        # for col in cols:
+        #     print(col)
+        
+        # dataList = [
+        #     {'name': 'Majeur', 'geom': 'SRID=4326;POLYGON((0 0,1 0,1 1,0 1,0 0))'},
+        #     {'name': 'Garde', 'geom': 'SRID=4326;POLYGON((1 0,3 0,3 2,1 2,1 0))'},
+        #     {'name': 'Orta', 'geom': 'SRID=4326;POLYGON((3 0,6 0,6 3,3 3,3 0))'}
+        # ]
+        # db.insert(lake_table, dataList)
+
+        # #  conn.execute(Poi.__table__.insert(), [
+        # #     {'geog': 'SRID=4326;POINT(1 1)'},
+        # #     {'geog': WKTElement('POINT(1 1)', srid=4326)},
+        # #     {'geog': WKTElement('SRID=4326;POINT(1 1)', extended=True)},
+        # #     {'geog': from_shape(Point(1, 1), srid=4326)}
+        # # ])
+
+
+        # sel_obj  = select([lake_table])
+        # result = db.select(sel_obj)
         # for row in result:
-        #     area = db.scalar(row['geom'].ST_Area())
-        #     wkt = db.scalar(row['geom'].ST_AsEWKT())
-        #     print(f"{row[0]} - {row[1]} - {area} - {wkt}")
+        #     geom = to_shape(row['geom'])
+        #     area = db.decode(row['geom'].ST_Area())
+        #     wkt = db.decode(row['geom'].ST_AsEWKT())
+        #     print(f"{row[0]} - {geom}  - {area} - {wkt}")
+        
+        # # result = db.query("select * from lake")
+        # # for row in result:
+        # #     area = db.scalar(row['geom'].ST_Area())
+        # #     wkt = db.scalar(row['geom'].ST_AsEWKT())
+        # #     print(f"{row[0]} - {row[1]} - {area} - {wkt}")
         
   
 
