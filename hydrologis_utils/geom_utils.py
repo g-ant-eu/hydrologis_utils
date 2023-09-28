b2d2d91939af806ab3f2443851bf25c8139d74c5
Andrea Antonello <andrea.antonello@gmail.com>
2023-09-28 14:37:16 +0200

    start adding geom utils

---
+++ hydrologis_utils/geom_utils.py
@@ -0,0 +1,56 @@
+from shapely.wkt import loads, dumps
+from shapely.wkb import loads as wkb_loads
+from shapely.ops import split,snap, transform, linemerge
+import numpy as np
+# import shapely geometry
+from shapely.geometry import GeometryCollection, LineString, MultiLineString, MultiPoint, MultiPolygon, Point, Polygon
+import shapely
+from shapely.geometry.base import BaseGeometry
+
+class HyGeomUtils():
+
+    # static method to convert geometry to 2D
+    @staticmethod
+    def convert2D( geom:BaseGeometry ) -> BaseGeometry:
+        if geom.has_z:
+            geom = transform(lambda x,y,z=None: (x,y), geom)
+        return geom
+    
+
+    @staticmethod
+    def splitLineEquidistant(line:LineString, distanceDelta:float = 3.0) -> list:
+        # generate the equidistant points
+        distances = np.arange(0, line.length, distanceDelta)
+        interpPoints = [line.interpolate(distance) for distance in distances]
+        interpPoints.append(line.boundary.geoms[1])
+        points = MultiPoint(interpPoints)
+        segmentsGc = split(snap(line, points, 1.0e-5), points)
+
+        linesList = [line for line in segmentsGc.geoms]
+        return linesList
+    
+    @staticmethod
+    def fromWkt(wkt:str) -> BaseGeometry:
+        return loads(wkt)
+    
+    @staticmethod
+    def toWkt(geom:BaseGeometry) -> str:
+        return dumps(geom)
+    
+    @staticmethod
+    def toWkb(geom:BaseGeometry) -> bytes:
+        return geom.wkb
+    
+    @staticmethod
+    def fromWkb(wkb:bytes) -> BaseGeometry:
+        return wkb_loads(wkb)
+    
+    @staticmethod
+    def joinLines(lines:any) -> any:
+        if isinstance(lines, list):
+            lines = MultiLineString(lines)
+        elif isinstance(lines, GeometryCollection):
+            lines = MultiLineString([line for line in lines.geoms])
+        
+        mergedLines = linemerge(lines)
+        return mergedLines
---
+++ tests/test_geom_utils.py
@@ -0,0 +1,83 @@
+from hydrologis_utils.geom_utils import HyGeomUtils
+
+import unittest
+
+# run with python3 -m unittest discover tests/
+
+class TestGeomutils(unittest.TestCase):
+    
+    # test fromWkt and toWkt methods
+    def test_wkt_conv(self):
+        # create a wkt of a Linstring
+        wkt = "LINESTRING (30 10, 10 30, 40 40)"
+        # convert to shapely geometry
+        geom = HyGeomUtils.fromWkt(wkt)
+
+        wkb = HyGeomUtils.toWkb(geom)
+        # convert back to geom
+        geom2 = HyGeomUtils.fromWkb(wkb)
+        
+        self.assertEqual(geom, geom2)
+
+
+    def test_geom_2d(self):
+        # create a 3d Linestring wkt
+        wkt = "LINESTRING Z (30 10 1, 10 30 2, 40 40 3)"
+        geom3D = HyGeomUtils.fromWkt(wkt)
+
+        # convert to 2d
+        geom2D = HyGeomUtils.convert2D(geom3D)
+
+        wkt2D = "LINESTRING (30 10, 10 30, 40 40)"
+        geom2Dexp = HyGeomUtils.fromWkt(wkt2D)
+
+        self.assertEqual(geom2D, geom2Dexp)
+
+    def test_split_line(self):
+        wkt = "LINESTRING (0 0, 10 0, 10 10, 18 10)"
+        geom = HyGeomUtils.fromWkt(wkt)
+        lines = HyGeomUtils.splitLineEquidistant(geom, 5)
+
+        self.assertEquals(len(lines), 6)
+
+        for line in lines[:-1]:
+            self.assertEquals(line.length, 5)
+        self.assertEquals(lines[-1].length, 3)
+        
+    # test joinLines method
+    def test_join_lines(self):
+        # create a wkt of a Linstring
+        wkt1 = "LINESTRING (30 10, 10 30, 40 40)"
+        wkt2 = "LINESTRING (40 40, 50 40, 50 30)"
+        wkt3 = "LINESTRING (41 40, 50 40, 50 30)"
+        joinWkt = "LINESTRING (30 10, 10 30, 40 40, 50 40, 50 30)"
+        # convert to shapely geometry
+        geom1 = HyGeomUtils.fromWkt(wkt1)
+        geom2 = HyGeomUtils.fromWkt(wkt2)
+        geom3 = HyGeomUtils.fromWkt(wkt3)
+        joinGeom = HyGeomUtils.fromWkt(joinWkt)
+
+        # join the two lines
+        lines = [geom1, geom2]
+        mergedLine = HyGeomUtils.joinLines(lines)
+
+        self.assertEqual(mergedLine.length,  geom1.length+geom2.length)
+        self.assertEqual(mergedLine, joinGeom)
+
+        # join the two lines
+        lines = [geom1, geom3]
+        mergedLine = HyGeomUtils.joinLines(lines)
+
+        self.assertEqual(len(mergedLine.geoms), 2)
+
+
+    
+
+
+    
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
