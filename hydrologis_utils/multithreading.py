59775d71fb4eb2383091aae60928781f1403450c
Andrea Antonello <andrea.antonello@gmail.com>
2023-09-28 10:47:16 +0200

    added multiprocessing

--- hydrologis_utils/multithreading.py
+++ hydrologis_utils/multithreading.py
@@ -5,10 +5,10 @@ Utilities to work with multithreading.
 import concurrent.futures
 import multiprocessing
 import threading
+from multiprocessing import Process, Manager, Pool
+from dill import dumps, loads
 
 class HyThreadingPool():
-
-
     def __init__(self, task, threads = None, name = "hythread") -> None:
         self.name = name
         self.index = 0
@@ -41,3 +41,59 @@ class HyThreadingPool():
 
             return results
     
+
+class HyMultiProcessing():
+    """
+    A class to run a function in parallel with multiprocessing.
+
+    Parameters
+    ----------
+    task: function
+        The function to run in parallel. This needs to be a toplevel function,
+        not include global variables that reference unpickable objects and 
+        use standard library functions.
+
+    cores: int, optional
+        The number of cores to use. If not given, all available cores are used.
+
+
+    """
+
+    def __init__(self, task, cores = None) -> None:
+        if not task:
+            raise Exception("No task given to run in parallel.")
+        else:
+            self.task = task
+
+        if not cores:
+            self.cores = multiprocessing.cpu_count()
+        else:
+            self.cores = cores
+        
+        multiprocessing.set_start_method("fork", force=True)
+        
+    def runParallel(self, paramsList:list):
+        """
+        Run a function in parallel with multiprocessing.
+        
+        Parameters
+        ----------
+        paramsList: list
+            A list of lists of parameters to pass in a loop to the function
+            and run in parallel.
+        
+        Returns
+        -------
+        results: list
+            A list of results from the function.
+        
+        """
+
+        pool = Pool(self.cores)
+        results = pool.map(self.task, paramsList)
+        pool.close()
+        pool.join()
+
+        processed_data = list(results)
+
+        return processed_data
--- tests/test_multithreading.py
+++ tests/test_multithreading.py
@@ -1,11 +1,24 @@
-from hydrologis_utils.multithreading import HyThreadingPool
+from hydrologis_utils.multithreading import HyThreadingPool, HyMultiProcessing
 
 import unittest
 
 # run with python3 -m unittest discover tests/
 
+def doMProcess(params:dict):
+    start = params["start"]
+    end = params["end"]
+
+    # make a multiplication and division in a loop between start and end
+    sum = 0
+    for i in range(start, end):
+        i = i * 2
+        i = i / 2
+        sum += i
+
+    return sum
+
 class TestMultithreading(unittest.TestCase):
-    def doProcess(self, params:dict):
+    def _doProcess(self, params:dict):
         start = params["start"]
         end = params["end"]
 
@@ -19,10 +32,7 @@ class TestMultithreading(unittest.TestCase):
         return sum
     
     def test_simple_multithreading(self):
-
-        pool = HyThreadingPool(task=self.doProcess)
-        
-
+        pool = HyThreadingPool(task=self._doProcess)
         count = 1000
         paramsList = []
         for i in range(0, count):
@@ -33,6 +43,21 @@ class TestMultithreading(unittest.TestCase):
             paramsList.append(params)
 
         results = pool.runThreads(paramsList)
+
+        self.assertEqual(len(results), count)
+
+    def test_simple_multiprocessing(self):
+        pool = HyMultiProcessing(task=doMProcess)
+        count = 1000
+        paramsList = []
+        for i in range(0, count):
+            params = {
+                "start": i,
+                "end": i+100
+            }
+            paramsList.append(params)
+
+        results = pool.runParallel(paramsList)
 
         self.assertEqual(len(results), count)
 
