0e12279e6b4a668adbaf153b8621502a9a302184
Andrea Antonello <andrea.antonello@gmail.com>
2024-01-10 15:23:05 +0100

    render lines and polygon tiles

--- hydrologis_utils/render_utils.py
+++ hydrologis_utils/render_utils.py
@@ -6,10 +6,18 @@ from hydrologis_utils.geom_utils import HyGeomUtils
 from hydrologis_utils.color_utils import HyColor
 
 class HyStyle():
-    def __init__(self, fillColor:HyColor, strokeColor:HyColor, strokeWidth:int):
+    def __init__(self, fillColor:HyColor = None, strokeColor:HyColor = None, strokeWidth:int = 2):
         self.fillColor = fillColor
         self.strokeColor = strokeColor
         self.strokeWidth = strokeWidth
+
+    @staticmethod
+    def defaultPolygonStyle():
+        return HyStyle(fillColor=HyColor(rgbaColor=(255,0,0,100)),strokeColor=HyColor(rgbaColor=(255,0,0,255)), strokeWidth=2)
+    
+    @staticmethod
+    def defaultLineStyle():
+        return HyStyle(strokeColor=HyColor(rgbaColor=(255,0,0,255)), strokeWidth=2)
 
 class HyGeomRenderer():
     """
@@ -17,33 +25,52 @@ class HyGeomRenderer():
     """
     def __init__(self, imageSize:(int, int)):
         self.imageSize = imageSize
-        self.polygonStyle = HyStyle(fillColor=HyColor(rgbaColor=(255,0,0,100)),strokeColor=HyColor(rgbaColor=(255,0,0,255)), strokeWidth=2)
+        self.polygonStyle = HyStyle.defaultPolygonStyle()
+        self.lineStyle = HyStyle.defaultLineStyle()
 
     def setPolygonStyle(self, style:HyStyle):
         self.polygonStyle = style
+    
+    def setLineStyle(self, style:HyStyle):
+        self.lineStyle = style
 
-    def renderTile(self, tileBoundsLongLat:[float], geometries:[BaseGeometry] ) -> Image:
+    def renderTile(self, tileBoundsLongLat:[float], geometries:[BaseGeometry] , antialias:bool=False) -> Image:
         # Create a bounding box geometry for the tile
         tile_box = box(tileBoundsLongLat[0], tileBoundsLongLat[1], tileBoundsLongLat[2], tileBoundsLongLat[3])
 
+
+        factor = 1
+        if antialias:
+            factor = 2
         # Create a transparent image
-        image = Image.new("RGBA", self.imageSize, (0, 0, 0, 0))
+        w = self.imageSize[0]*factor
+        h = self.imageSize[1]*factor
+        
+        image = Image.new("RGBA", (w, h), (0, 0, 0, 0))
         # Create a drawing object
         draw = ImageDraw.Draw(image)
 
         for geom in geometries:
             if geom.intersects(tile_box):
-                matrix = HyGeomUtils.worldToRectangleMatrix([tileBoundsLongLat[0], tileBoundsLongLat[1], tileBoundsLongLat[2], tileBoundsLongLat[3]], [0, 0, self.imageSize[0], self.imageSize[1]])
+                matrix = HyGeomUtils.worldToRectangleMatrix([tileBoundsLongLat[0], tileBoundsLongLat[1], tileBoundsLongLat[2], tileBoundsLongLat[3]], [0, 0, w, h])
                 scaledGeom = affine_transform(geom, matrix)
 
                 if scaledGeom.geom_type == "Polygon":
-                    draw.polygon(scaledGeom.exterior.coords, outline=self.polygonStyle.strokeColor.getRgba(), fill=self.polygonStyle.fillColor.getRgba(), width=self.polygonStyle.strokeWidth)
+                    draw.polygon(scaledGeom.exterior.coords, outline=self.polygonStyle.strokeColor.getRgba(), fill=self.polygonStyle.fillColor.getRgba(), width=self.polygonStyle.strokeWidth*factor)
                     for hole in scaledGeom.interiors:
-                        draw.polygon(hole.coords, outline=self.polygonStyle.strokeColor.getRgba(), fill=(0, 0, 0, 0), width=self.polygonStyle.strokeWidth)
+                        draw.polygon(hole.coords, outline=self.polygonStyle.strokeColor.getRgba(), fill=(0, 0, 0, 0), width=self.polygonStyle.strokeWidth*factor)
                 elif scaledGeom.geom_type == "MultiPolygon":
                     for poly in scaledGeom.geoms:
-                        draw.polygon(poly.exterior.coords, outline=self.polygonStyle.strokeColor.getRgba(), fill=self.polygonStyle.fillColor.getRgba(), width=self.polygonStyle.strokeWidth)
+                        draw.polygon(poly.exterior.coords, outline=self.polygonStyle.strokeColor.getRgba(), fill=self.polygonStyle.fillColor.getRgba(), width=self.polygonStyle.strokeWidth*factor)
                         for hole in poly.interiors:
-                            draw.polygon(hole.coords, outline=self.polygonStyle.strokeColor.getRgba(), fill=(0, 0, 0, 0), width=self.polygonStyle.strokeWidth)
+                            draw.polygon(hole.coords, outline=self.polygonStyle.strokeColor.getRgba(), fill=(0, 0, 0, 0), width=self.polygonStyle.strokeWidth*factor)
+                elif scaledGeom.geom_type == "LineString":
+                    draw.line(scaledGeom.coords, fill=self.polygonStyle.strokeColor.getRgba(), width=self.polygonStyle.strokeWidth*factor)
+                elif scaledGeom.geom_type == "MultiLineString":
+                    for line in scaledGeom.geoms:
+                        draw.line(line.coords, fill=self.polygonStyle.strokeColor.getRgba(), width=self.polygonStyle.strokeWidth*factor)
+
+        if antialias:
+            image = image.resize(self.imageSize, Image.Resampling.LANCZOS)
 
         return image
--- tests/test_render_utils.py
+++ tests/test_render_utils.py
@@ -3,7 +3,7 @@ sys.path.append("./")
 from hydrologis_utils.render_utils import HyGeomRenderer, HyStyle
 from hydrologis_utils.geom_utils import HyGeomUtils
 from hydrologis_utils.color_utils import HyColor
-from shapely.geometry import Polygon, Point, box
+from PIL import Image, ImageChops
 
 import unittest
 
@@ -24,13 +24,46 @@ class TestRenderUtils(unittest.TestCase):
                 
         boundsGeom = HyGeomUtils.fromWkt("POLYGON ((200 700, 900 700, 900 0, 200 0, 200 700))")
         tile_bounds = boundsGeom.bounds
-        # tile_bounds = (200, 0, 900, 700)  # (min_lat, min_lon, max_lat, max_lon)
 
         renderer = HyGeomRenderer(imageSize=(512, 512))
         renderer.setPolygonStyle(HyStyle(fillColor=HyColor(rgbaColor=(0,0,255,128)),strokeColor=HyColor(rgbaColor=(0,0,255,255)), strokeWidth=5))
         tileImage = renderer.renderTile( tile_bounds, [geom1, geom2])
 
-        tileImage.show()
+        compareImage = "./tests/samples/test_tile_polygon.png"
+        # compare the image with this
+        diff = ImageChops.difference(tileImage, Image.open(compareImage))
+        self.assertIsNone(diff.getbbox())
+
+        tileImage = renderer.renderTile( tile_bounds, [geom1, geom2], antialias=True)
+        compareImage = "./tests/samples/test_tile_polygon_antialias.png"
+        # compare the image with this
+        diff = ImageChops.difference(tileImage, Image.open(compareImage))
+        self.assertIsNone(diff.getbbox())
+
+
+
+    
+    def test_line_tile(self):
+        geom1Wkt = """MULTILINESTRING ((160 -30, 460 360, 110 450, 360 750, 570 530, 860 740, 1090 440, 630 360), 
+                        (460 70, 510 340, 735 116, 770 490))"""
+        geom2Wkt = """LINESTRING (312 205, 490 520, 570 80, 790 600)"""
+        
+        geom1 = HyGeomUtils.fromWkt(geom1Wkt)
+        geom2 = HyGeomUtils.fromWkt(geom2Wkt)
+                
+        boundsGeom = HyGeomUtils.fromWkt("POLYGON ((200 700, 900 700, 900 0, 200 0, 200 700))")
+        tile_bounds = boundsGeom.bounds
+
+        renderer = HyGeomRenderer(imageSize=(512, 512))
+        renderer.setLineStyle(HyStyle(strokeColor=HyColor(rgbaColor=(0,0,255,255)), strokeWidth=5))
+        tileImage = renderer.renderTile( tile_bounds, [geom1, geom2], antialias=True)
+
+        compareImage = "./tests/samples/test_tile_line.png"
+        tileImage.save(compareImage)
+        # compare the image with this
+        # diff = ImageChops.difference(tileImage, Image.open(compareImage))
+        # self.assertIsNone(diff.getbbox())
+
 
 
 if __name__ == "__main__":
