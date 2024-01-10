e2cab863d5a5dd2289642c5ca82ccc497625371e
Andrea Antonello <andrea.antonello@gmail.com>
2024-01-10 17:52:13 +0100

    misch test

--- tests/test_render_utils.py
+++ tests/test_render_utils.py
@@ -11,16 +11,21 @@ import unittest
 # run with python3 -m unittest discover tests/
 
 class TestRenderUtils(unittest.TestCase):
+    polygon1Wkt = """MULTIPOLYGON (((110 420, 305 274, 580 480, 350 750, 140 720, 110 420)), 
+                ((460 280, 566 115, 766 195, 690 410, 460 280)), 
+                ((760 720, 950 140, 1120 710, 760 720)))"""
+    polygon2Wkt = """POLYGON ((300 -30, 280 160, 640 180, 660 40, 300 -30), 
+                (384 124, 400 30, 585 66, 570 140, 384 124))"""
+    line1Wkt = """MULTILINESTRING ((160 -30, 460 360, 110 450, 360 750, 570 530, 860 740, 1090 440, 630 360), 
+                        (460 70, 510 340, 735 116, 770 490))"""
+    line2Wkt = """LINESTRING (312 205, 490 520, 570 80, 790 600)"""
+    point1Wkt = """MULTIPOINT ((290 160), (460 410), (555.5555555555555 244.44444444444443), (925.5555555555555 436.66666666666663))"""
+    point2Wkt = """POINT (760 40)"""
     
     def test_polygon_tile(self):
-        geom1Wkt = """MULTIPOLYGON (((110 420, 305 274, 580 480, 350 750, 140 720, 110 420)), 
-                    ((460 280, 566 115, 766 195, 690 410, 460 280)), 
-                    ((760 720, 950 140, 1120 710, 760 720)))"""
-        geom2Wkt = """POLYGON ((300 -30, 280 160, 640 180, 660 40, 300 -30), 
-                    (384 124, 400 30, 585 66, 570 140, 384 124))"""
         
-        geom1 = HyGeomUtils.fromWkt(geom1Wkt)
-        geom2 = HyGeomUtils.fromWkt(geom2Wkt)
+        geom1 = HyGeomUtils.fromWkt(self.polygon1Wkt)
+        geom2 = HyGeomUtils.fromWkt(self.polygon2Wkt)
                 
         boundsGeom = HyGeomUtils.fromWkt("POLYGON ((200 700, 900 700, 900 0, 200 0, 200 700))")
         tile_bounds = boundsGeom.bounds
@@ -52,12 +57,8 @@ class TestRenderUtils(unittest.TestCase):
 
     
     def test_line_tile(self):
-        geom1Wkt = """MULTILINESTRING ((160 -30, 460 360, 110 450, 360 750, 570 530, 860 740, 1090 440, 630 360), 
-                        (460 70, 510 340, 735 116, 770 490))"""
-        geom2Wkt = """LINESTRING (312 205, 490 520, 570 80, 790 600)"""
-        
-        geom1 = HyGeomUtils.fromWkt(geom1Wkt)
-        geom2 = HyGeomUtils.fromWkt(geom2Wkt)
+        geom1 = HyGeomUtils.fromWkt(self.line1Wkt)
+        geom2 = HyGeomUtils.fromWkt(self.line2Wkt)
                 
         boundsGeom = HyGeomUtils.fromWkt("POLYGON ((200 700, 900 700, 900 0, 200 0, 200 700))")
         tile_bounds = boundsGeom.bounds
@@ -71,12 +72,9 @@ class TestRenderUtils(unittest.TestCase):
         diff = ImageChops.difference(tileImage, Image.open(compareImage))
         self.assertIsNone(diff.getbbox())
     
-    def test_point_tile(self):
-        geom1Wkt = """MULTIPOINT ((290 160), (460 410), (555.5555555555555 244.44444444444443), (925.5555555555555 436.66666666666663))"""
-        geom2Wkt = """POINT (760 40)"""
-        
-        geom1 = HyGeomUtils.fromWkt(geom1Wkt)
-        geom2 = HyGeomUtils.fromWkt(geom2Wkt)
+    def test_point_tile(self):     
+        geom1 = HyGeomUtils.fromWkt(self.point1Wkt)
+        geom2 = HyGeomUtils.fromWkt(self.point2Wkt)
                 
         boundsGeom = HyGeomUtils.fromWkt("POLYGON ((200 700, 900 700, 900 0, 200 0, 200 700))")
         tile_bounds = boundsGeom.bounds
@@ -95,6 +93,38 @@ class TestRenderUtils(unittest.TestCase):
         diff = ImageChops.difference(tileImage, Image.open(compareImage))
         self.assertIsNone(diff.getbbox())
 
+    def test_misch_tile(self):     
+        geom1 = HyGeomUtils.fromWkt(self.polygon1Wkt)
+        geom2 = HyGeomUtils.fromWkt(self.polygon2Wkt)
+        geom3 = HyGeomUtils.fromWkt(self.line1Wkt)
+        geom4 = HyGeomUtils.fromWkt(self.line2Wkt)
+        geom5 = HyGeomUtils.fromWkt(self.point1Wkt)
+        geom6 = HyGeomUtils.fromWkt(self.point2Wkt)
+                
+        boundsGeom = HyGeomUtils.fromWkt("POLYGON ((200 700, 900 700, 900 0, 200 0, 200 700))")
+        tile_bounds = boundsGeom.bounds
+
+        renderer = HyGeomRenderer(imageSize=(512, 512))
+        
+        # style by attribute
+        colorTable = {
+            "points":HyStyle(fillColor=HyColor(rgbaColor=(255,0,0,180)),strokeColor=HyColor(rgbaColor=(255,0,0,255)), strokeWidth=1, size=30), 
+            "lines":HyStyle(strokeColor=HyColor(rgbaColor=(0,255,0,255)), strokeWidth=3),
+            "polygons":HyStyle(fillColor=HyColor(rgbaColor=(0,0,255,180)),strokeColor=HyColor(rgbaColor=(0,0,255,255)), strokeWidth=2), 
+        }
+        tileImage = renderer.renderTile( tile_bounds, [
+            ExtendedGeometry(geom1, "polygons"), 
+            ExtendedGeometry(geom2, "polygons"),
+            ExtendedGeometry(geom3, "lines"), 
+            ExtendedGeometry(geom4, "lines"),
+            ExtendedGeometry(geom5, "points"), 
+            ExtendedGeometry(geom6, "points"),
+            ], colorTable=colorTable, antialias=True)
+
+        compareImage = "./tests/samples/test_tile_misch.png"
+        # compare the image with this
+        diff = ImageChops.difference(tileImage, Image.open(compareImage))
+        self.assertIsNone(diff.getbbox())
 
 
 if __name__ == "__main__":
